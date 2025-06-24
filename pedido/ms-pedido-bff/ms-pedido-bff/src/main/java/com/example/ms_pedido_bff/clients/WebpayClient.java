38d0d6c345a1eb77bc4fcb235ed0d652fe2e851b
jossy2430 <134448658+jossy2430@users.noreply.github.com>
2025-06-23 22:52:56 -0400

    Update WebpayClient.java

--- pedido/ms-pedido-bff/ms-pedido-bff/src/main/java/com/example/ms_pedido_bff/clients/WebpayClient.java
+++ pedido/ms-pedido-bff/ms-pedido-bff/src/main/java/com/example/ms_pedido_bff/clients/WebpayClient.java
@@ -1,9 +1,14 @@
 package com.example.ms_pedido_bff.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.example.ms_pedido_bff.modelDto.WebpayCommitResponseDTO;
+import com.example.ms_pedido_bff.modelDto.WebpayStatusResponseDTO;
 import com.example.ms_pedido_bff.modelDto.WebpayTransactionDTO;
 
 @FeignClient(name = "webpayClient", url = "${webpay.api.url}")
@@ -11,4 +16,10 @@ public interface WebpayClient {
 
     @PostMapping("/transactions")
     WebpayTransactionDTO createTransaction(@RequestBody WebpayTransactionDTO request);
+
+    @PutMapping("/transactions/{token}")
+    WebpayCommitResponseDTO confirmTransaction(@PathVariable("token")String token);
+
+    @GetMapping("/transactions/{token}")
+WebpayStatusResponseDTO getTransactionStatus(@PathVariable("token") String token);
 }
