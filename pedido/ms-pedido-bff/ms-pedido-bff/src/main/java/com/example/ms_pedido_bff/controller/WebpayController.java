1ef7bcf37502f2aca07613cceab8a7713c65b6e1
jossy2430 <134448658+jossy2430@users.noreply.github.com>
2025-06-23 22:53:35 -0400

    Update WebpayController.java

--- pedido/ms-pedido-bff/ms-pedido-bff/src/main/java/com/example/ms_pedido_bff/controller/WebpayController.java
+++ pedido/ms-pedido-bff/ms-pedido-bff/src/main/java/com/example/ms_pedido_bff/controller/WebpayController.java
@@ -2,6 +2,8 @@ package com.example.ms_pedido_bff.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import com.example.ms_pedido_bff.modelDto.WebpayCommitResponseDTO;
+import com.example.ms_pedido_bff.modelDto.WebpayStatusResponseDTO;
 import com.example.ms_pedido_bff.modelDto.WebpayTransactionDTO;
 import com.example.ms_pedido_bff.service.WebpayService;
 
@@ -19,4 +21,16 @@ public class WebpayController {
     public WebpayTransactionDTO createTransaction(@RequestBody WebpayTransactionDTO request) {
         return webpayService.createTransaction(request);
     }
+
+    @PutMapping("/transactions/{token}/commit")
+    public WebpayCommitResponseDTO confirmTransaction(@PathVariable String token) {
+        return webpayService.confirmTransaction(token);
+    }
+    
+    @GetMapping("/transactions/{token}/status")
+    public WebpayStatusResponseDTO getTransactionStatus(@PathVariable String token) {
+        return webpayService.getTransactionStatus(token);
+    }
+
+    
 }
